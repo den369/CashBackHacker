933e2aaf8035f736e2b922bc2840e784f2d5cf12
Denis Petrovskii <denpetrovsky369@gmail.com>
2022-12-14 05:07:35 +0300

    first_testng_commit

---
+++ src/test/java/ru/netology/CashBackHackerServiceTest.java
@@ -0,0 +1,44 @@
+package ru.netology;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+public class CashBackHackerServiceTest {
+
+    CashBackHackerService service = new CashBackHackerService();
+
+
+    @Test
+    public void showUnderBoundary() {
+        int amount = 700;
+        int actual = service.remain(amount);
+        int expected = 300;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void showAboveBoundary() {
+        int amount = 1450;
+        int actual = service.remain(amount);
+        int expected = 550;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void showZeroAmount() {
+        int amount = 0;
+        int actual = service.remain(amount);
+        int expected = 1000;
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void showEqualsBoundary() {
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+
+}
